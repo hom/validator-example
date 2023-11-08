def790d4670f290bd03b95f02fea434b46ddf882
mengj <813935532@qq.com>
2023-11-09 00:35:46 +0800

    feat: transform

--- src/app/app.dto.ts
+++ src/app/app.dto.ts
@@ -1,18 +1,35 @@
 import {
   IsArray,
+  IsEnum,
   IsNumber,
   IsObject,
   IsString,
   Min,
   ValidateNested,
 } from 'class-validator';
-import { Type, TypeHelpOptions } from 'class-transformer';
+import {
+  // Transform,
+  Type,
+  TypeHelpOptions,
+  // plainToInstance,
+} from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class CatDto {
+export class BaseDto {
   @ApiProperty()
   @IsString()
   name: string;
+}
+
+export class CatDto extends BaseDto {
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsEnum(['cat'])
+  type: 'cat';
 
   @ApiProperty()
   @IsNumber()
@@ -20,10 +37,15 @@ export class CatDto {
   age: number;
 }
 
-export class DogDto {
+export class DogDto extends BaseDto {
   @ApiProperty()
   @IsString()
   name: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsEnum(['dog'])
+  type: 'dog';
 
   @ApiProperty()
   @IsNumber()
@@ -40,15 +62,40 @@ export class ZooDto {
   @IsArray()
   @IsObject({ each: true })
   @ValidateNested({ each: true })
-  @Type((data: TypeHelpOptions) => {
-    const { object, property } = data;
-    console.log(
-      '🚀 ~ file: app.dto.ts:45 ~ ZooDto ~ @Type ~ property:',
-      property,
-    );
-    console.log('🚀 ~ file: app.dto.ts:45 ~ ZooDto ~ @Type ~ object:', object);
-    return CatDto;
+  // @Type((data: TypeHelpOptions) => {
+  //   const { object, property } = data;
+  //   console.log(
+  //     '🚀 ~ file: app.dto.ts:45 ~ ZooDto ~ @Type ~ property:',
+  //     property,
+  //   );
+  //   console.log('🚀 ~ file: app.dto.ts:45 ~ ZooDto ~ @Type ~ object:', object);
+  //   return CatDto;
+  // })
+  @Type(() => BaseDto, {
+    keepDiscriminatorProperty: true,
+    discriminator: {
+      property: 'type',
+      subTypes: [
+        { value: CatDto, name: 'cat' },
+        { value: DogDto, name: 'dog' },
+      ],
+    },
   })
+  // @Transform(
+  //   (object) => {
+  //     console.log('🚀 ~ file: app.dto.ts:54 ~ ZooDto ~ object:', object);
+  //     const { value } = object;
+  //     console.log(
+  //       '🚀 ~ file: app.dto.ts:53 ~ ZooDto ~ @Transform ~ value:',
+  //       value,
+  //     );
+  //     return value.map((item) => {
+  //       if (item.type === 'cat') return plainToInstance(CatDto, item);
+  //       if (item.type === 'dog') return plainToInstance(DogDto, item);
+  //     });
+  //   },
+  //   // { toClassOnly: true },
+  // )
   animals: (CatDto | DogDto)[];
 }
 
